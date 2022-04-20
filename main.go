2152049e5c0fd0839791a0beccce49d6114fff7f
Clark Ritchie <clarkritchie@users.noreply.github.com>
2022-04-20 09:16:37 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
-import "C"
-import "fmt"
-
-// func Add(x, y int) int {
-//     fmt.Printf("Go says: adding %v and %v\n", x, y)
-//     return x + y
-// }
+import (
+    "C"
+    "fmt"
+)
 
 //export Sieve
 func Sieve(n int) {
